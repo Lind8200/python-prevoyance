da7d49341b7f8883ba04916cbf8fc762ddb35e3d
Lind8200 <linda.scheveiler@axa.fr>
2025-10-27 11:42:53 +0100

    Initi commit

---
+++ Mainprev.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 26 14:47:39 2020
+
+@author: Linda Scheveiler
+"""
+
+import vues.Menu_prev as vue
+
+class Mainprev():
+
+    if __name__== '__main__':
+
+        menu_prev = vue.Menu_prev()
+        menu_prev.MainprevMenu()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
